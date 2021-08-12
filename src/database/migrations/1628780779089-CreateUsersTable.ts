94dd8fd179fb733ae96eda67dac1662444a52865
Alessandro Lins <aflinsjr@icloud.com>
2021-08-12 12:14:05 -0300

    migrations created successfully

---
+++ src/database/migrations/1628780779089-CreateUsersTable.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersTable1628780779089 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+          await queryRunner.createTable(new Table({
+            name: "users",
+            columns: [
+              {
+                name: "id",
+                type: "uuid",
+                isPrimary: true,
+                generationStrategy: "uuid",
+                default: 'uuid_generate_v4()'
+              },
+
+              {
+                name: "nome",
+                type: "varchar",
+                isUnique: true,
+              },
+
+              {
+                name: "cidade",
+                type: "varchar",
+              },
+
+              {
+                name: "uf",
+                type: "varchar",
+              },
+
+              {
+                name: "ddd",
+                type: "varchar",
+              },
+
+              {
+                name: "created_at",
+                type: "timestamp",
+                default: 'now()'
+              },
+            ],
+          })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("users");
+         await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+    }
+}
